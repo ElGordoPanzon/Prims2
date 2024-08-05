cee3eb4a11fd9ec29dc0367b9697e07b133fa1a3
jose jimenez <jimenjos000@warren.k12.in.us>
2024-08-05 12:52:52 -0400

    Now I has code

---
+++ src/Prims.java
@@ -0,0 +1,31 @@
+public class Prims {
+    public static void main(String[] args) {
+
+        byte myMinByteValue = -128;
+        //byte my NewByteValue = =128 / 2;
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        int myNewInt = myMinByteValue;
+
+        float myFloatValue = 23.235436464F;
+        float myFloatValue2 = 1.4e-3F;   // 1.4 8 10^-3
+        System.out.println(myFloatValue);
+        System.out.println(myFloatValue2);
+
+        double myDoubleValue = -78.6489237412;
+        double myDoubleValue2 = 1.8e14;
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        double answer = 3.0 / 2.0;
+        System.out.println(answer);
+
+        System.out.println(0.1 + 0.1 + 0.1);
+
+    }
+}
